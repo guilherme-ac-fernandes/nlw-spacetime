5c1f8a81a6e3cac5eeac9899e2c9e0d5bdfa80b7
Guilherme Fernandes <gfengquimica@gmail.com>
2023-05-19 19:38:39 -0300

    feat: aplicação do CRUD para memorias + configuração do CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,14 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
-// import { memoriesRoutes } from './routes/memories';
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 app.register(cors, {
   origin: true,
 })
 
-// app.register(memoriesRoutes);
-
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
+app.register(memoriesRoutes)
 
 const serverMessage: string =
   '🚀 HTTP server running on port http://localhost:3333'
