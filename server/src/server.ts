e22e46f8253a92cbe57932133a5a0528c0fcd3fc
Guilherme Fernandes <gfengquimica@gmail.com>
2023-07-06 23:17:31 -0300

    feat: criação da rota de registro de usuário e retorno do token

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,9 +12,19 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'nlw_spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 const serverMessage: string =
   '🚀 HTTP server running on port http://localhost:3333'
 
-app.listen({ port: 3333 }).then(() => console.log(serverMessage))
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => console.log(serverMessage))
